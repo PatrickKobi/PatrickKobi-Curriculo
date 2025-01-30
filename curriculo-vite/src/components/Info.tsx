f235c720481c0a4e2f798b34a4c2d3232709e376
Patrick Kobi <patrickpkttt@gmail.com>
2025-01-30 12:28:37 -0300

    Components
    Info

---
+++ curriculo-vite/src/components/Info.tsx
@@ -0,0 +1,11 @@
+
+const Informacoes = () => {
+    return (
+        <div>
+            <h1>Informação</h1>
+            <p>Texto de informação</p>
+        </div>
+    )
+}
+
+export default Informacoes;
