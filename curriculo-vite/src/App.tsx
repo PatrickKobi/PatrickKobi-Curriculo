25ec689c2a19cd1f8b2cbbe63f71b7ea1f451b23
Patrick Kobi <patrickpkttt@gmail.com>
2025-01-30 23:45:04 -0300

    e

--- curriculo-vite/src/App.tsx
+++ curriculo-vite/src/App.tsx
@@ -1,34 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+const App = () => {
 
-function App() {
-  const [count, setCount] = useState(0)
+  const name: string = 'Patrick Kobi'
+  //const age: number = 19
+  const ocupation: string = 'Desenvolvedor Web'
+  const endereco: string = 'Rua Nossa Senhora das Graças, Duque de Caxias/RJ'
+  const email: string = 'patrickpkttt@gmail.com'
+  const telefone: string = '(21) 96805-5205'
+  const linkedin: string = 'https://www.linkedin.com/in/patrickkobi/'
+  //const foto: string = 'https://avatars.githubusercontent.com/u/77445900?v=4'
+
+  const desc: string = 'Sou um jovem apaixonado portecnologia, com interesse em programação e inovação digital. Me destaco pela rápida aprendizagem e habilidades práticas em desenvolvimento de apps e websites. Sempre em busca de novos desafios, eu busco oportunidades para expandir meus conhecimentos e contribuir para soluções tecnológicas criativas e eficientes.'
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        GIRA ASPAS
-      </p>
-    </>
+    <div>
+      <h2>{name}</h2>
+      <h2>{ocupation}</h2>
+      <h2>{endereco}</h2>
+      <h2>{email}</h2>
+      <h2>{telefone}</h2>
+      <h2><a href={linkedin} target="_blank">{linkedin}</a></h2>
+      <h2>{desc}</h2>
+    </div>
   )
 }
 
